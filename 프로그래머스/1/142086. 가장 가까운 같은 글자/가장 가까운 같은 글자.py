bc54e7ad9e1aa31e76c622a18647c69b6c09a38a
rabongee <rmarkdehdals@gmail.com>
2024-07-12 10:42:13 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.84 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -2,10 +2,6 @@ def solution(s):
     answer = []
     word_dict={}
     for idx, word in enumerate(s):
-        if word not in word_dict:
-            answer.append(-1)
-            word_dict[word] = idx
-        else:
-            answer.append(idx-word_dict[word])
-            word_dict[word] = idx
+        answer.append(-1) if word not in word_dict else answer.append(idx-word_dict[word])
+        word_dict[word] = idx
     return answer
