0f67f8c08c7fc39eefb25dd369416c689f353900
rabongee <rmarkdehdals@gmail.com>
2024-07-12 10:33:53 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.43 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = []
+    word_dict={}
+    for idx, word in enumerate(s):
+        if word not in word_dict:
+            answer.append(-1)
+            word_dict[word] = idx
+        else:
+            answer.append(idx-word_dict[word])
+            word_dict[word] = idx
+    return answer
