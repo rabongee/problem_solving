dc591c1a8e88d75a8ae93cd1dc1b6a64f110555a
rabongee <rmarkdehdals@gmail.com>
2024-07-13 18:24:05 +0900

    [level 1] Title: 콜라 문제, Time: 0.67 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,7 +1,6 @@
 def solution(a, b, n):
     answer = 0
     while n >= a:
-        n -= a
-        n += b
-        answer += b
+        answer += (n//a) * b
+        n = ((n//a) * b) + (n % a)
     return answer
