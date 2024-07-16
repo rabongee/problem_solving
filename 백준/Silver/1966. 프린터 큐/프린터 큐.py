9f5a21a8e2c4a78ad494f0a62a83cdc13f65fecf
rabongee <rmarkdehdals@gmail.com>
2024-07-16 18:38:50 +0900

    [Silver III] Title: 프린터 큐, Time: 76 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,27 +1,30 @@
 from collections import deque
 
-testcase = int(input())
-
-for _ in range(testcase):
+def solution(d, p, r):
     count = 0
-    document, position = map(int, input().split(" "))
-    ranks = list(map(int,input().split(" ")))
     print_queue = deque()
-    for i, rank in enumerate(ranks):
-        d = {
+    for i, rank in enumerate(r):
+        paper = {
             "index": i,
             "rank": rank,
         }
-        print_queue.append(d)
+        print_queue.append(paper)
     while print_queue:
-        first = print_queue.popleft() # 0, 1
-        for queue in print_queue: # 1, 2
+        first = print_queue.popleft()
+        for queue in print_queue:
             if first["rank"] < queue["rank"]:
                 print_queue.append(first)
                 break
         if first in print_queue:
             continue
         count += 1
-        if first["index"] == position:
+        if first["index"] == p:
             print(count)
             break
+
+
+testcase = int(input())
+for _ in range(testcase):
+    document, position = map(int, input().split(" "))
+    ranks = list(map(int,input().split(" ")))
+    solution(document, position, ranks)
