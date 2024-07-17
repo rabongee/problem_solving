c4d6cac87b00b9c8c5cf995192b7104cc289bcd6
rabongee <rmarkdehdals@gmail.com>
2024-07-17 19:20:46 +0900

    [Silver II] Title: 최소 힙, Time: 104 ms, Memory: 37044 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,40 +1,14 @@
 import sys
-
-
-def percolate_down(arr, cur):
-    smallest = cur
-    left = 2 * cur
-    right = 2 * cur + 1
-
-    if left <= len(arr) - 1 and arr[left] < arr[smallest]:
-        smallest = left
-
-    if right <= len(arr) - 1 and arr[right] < arr[smallest]:
-        smallest = right
-
-    if smallest != cur:
-        arr[cur], arr[smallest] = arr[smallest], arr[cur]
-        percolate_down(arr, smallest)
-
+import heapq
 
 n = int(input())
-arr = [None]
+heap = []
 for _ in range(n):
     x = int(sys.stdin.readline())
     if x == 0:
-        if len(arr)-1 < 1:
+        if not heap:
             print(0)
         else:
-            arr[1], arr[-1] = arr[-1], arr[1]
-            print(arr.pop())
-            percolate_down(arr,1)
-
+            print(heapq.heappop(heap))
     else:
-        arr.append(x)
-        cur = len(arr) - 1
-        parent = cur // 2
-        while parent > 0:
-            if arr[cur] < arr[parent]:
-                arr[parent], arr[cur] = arr[cur], arr[parent]
-            cur = parent
-            parent = cur // 2
+        heapq.heappush(heap, x)
