8fbbea2a7fcb83ae2608fd748c35d8774428b796
rabongee <rmarkdehdals@gmail.com>
2024-07-18 09:42:52 +0900

    [level 1] Title: 시저 암호, Time: 2.96 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -4,7 +4,7 @@ def solution(s, n):
         if i == " ":
             answer += i
         elif i.isupper():
-            answer += chr(ord(i) + n) if (ord(i) + n) <= ord('Z') else chr(ord(i) + n - 26)
+            answer += chr(((ord(i)-ord('A')+n) % 26) + ord('A'))
         else:
-            answer += chr(ord(i) + n) if (ord(i) + n) <= ord('z') else chr(ord(i) + n - 26)
+            answer += chr(((ord(i)-ord('a')+n) % 26) + ord('a'))
     return answer
