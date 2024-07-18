def926f112a1c6640b26480a6b09ff750f9bf7ae
rabongee <rmarkdehdals@gmail.com>
2024-07-18 21:52:52 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,17 +1,14 @@
 def solution(lottos, win_nums):
     answer = []
-    win_dict = {}
-    rank = {0:6,1:6,2:5,3:4,4:3,5:2,6:1}
     max_count = 0
     min_count = 0
-    for num in win_nums:
-        win_dict[num] = True
-    for lotto in lottos:
-        if win_dict.get(lotto):
-            min_count += 1
-        elif lotto == 0:
-            max_count += 1
+    rank = [6,6,5,4,3,2,1]
+    for num in lottos:
+        if num in win_nums:
+            min_count +=1
+        elif num == 0:
+            max_count +=1
     max_count += min_count
-    answer.append(rank.get(max_count))
-    answer.append(rank.get(min_count))
+    answer.append(rank[max_count])
+    answer.append(rank[min_count])
     return answer
