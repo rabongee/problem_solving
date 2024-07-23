df7e8376799ce1db9c3ad7dd08626b45636c2b68
rabongee <rmarkdehdals@gmail.com>
2024-07-23 10:14:55 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,29 +1,11 @@
 def solution(n, lost, reserve):
-    answer = 0
-    student = {}
-    for i in range(1,n+1):
-        student[i] = 1
-        if i in lost:
-            student[i] -= 1
-        if i in reserve:
-            student[i] += 1
-    for key in student.keys():
-        if key != 1 or key != n:
-            if student.get(key) > 1 and student.get(key-1) == 0:
-                student[key] -= 1
-                student[key - 1] += 1
-            elif student.get(key) > 1 and student.get(key+1) == 0:
-                student[key] -= 1
-                student[key + 1] += 1
-        elif key == 1:
-            if student.get(key) > 1 and student.get(key + 1) == 0:
-                student[key] -= 1
-                student[key + 1] += 1
-        elif key == n:
-            if student.get(key) > 1 and student.get(key-1) == 0:
-                student[key] -= 1
-                student[key - 1] += 1
-    for value in student.values():
-        if value > 0:
-            answer += 1
-    return answer
+    lost_set = set(lost) - set(reserve)
+    reserve_set = set(reserve) - set(lost)
+
+    for student in sorted(reserve_set):
+        if student - 1 in lost_set:
+            lost_set.remove(student - 1)
+        elif student + 1 in lost_set:
+            lost_set.remove(student + 1)
+
+    return n - len(lost_set)
