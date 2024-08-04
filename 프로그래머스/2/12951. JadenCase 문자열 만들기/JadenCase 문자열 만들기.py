da330d61639026c08b3a635466bf37609a9a0570
rabongee <rmarkdehdals@gmail.com>
2024-08-04 22:58:18 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,11 +1,10 @@
 def solution(s):
     answer = ''
     s = s.title()
-    s_list = list(s)
-    num_str = {'0','1','2','3','4','5','6','7','8','9'}
-    for i in range(len(s_list)-1):
-        if s_list[i] in num_str:
-            s_list[i+1] = s_list[i+1].lower()
-        answer += s_list[i]
-    answer += s_list[-1]
+    num_str = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+    for i in range(len(s)):
+        if i > 0 and s[i - 1] in num_str:
+            answer += s[i].lower()
+        else:
+            answer += s[i]
     return answer
