98cb69bc4d40c2c83c882883fa5cb76a6ce76099
rabongee <rmarkdehdals@gmail.com>
2024-08-08 09:45:36 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,14 +1,13 @@
 def solution(arr):
-    arr.sort(reverse=True)
-    multi = arr[0]
-    for i in range(1,len(arr)):
-        num_1 = multi
+    answer = arr[-1]
+    for i in range(len(arr)-2, -1, -1):
+        num_1 = answer
         num_2 = arr[i]
         while True:
             divisor = num_1 % num_2
             if divisor == 0:
-                multi = multi * arr[i] // num_2
+                answer = answer * arr[i] // num_2
                 break
             num_1 = num_2
             num_2 = divisor
-    return multi
+    return answer
